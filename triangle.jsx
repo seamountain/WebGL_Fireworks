b29239e6a4a3d4d92294e3c1091da6623889d5ed
sea_mountain <seaforestmountain@gmail.com>
2013-08-25 00:54:16 +0900

    Add HSV converter

--- triangle.jsx
+++ triangle.jsx
@@ -86,6 +86,29 @@ class _Main {
       colors.splice(0, dataNum);
     }
 
+    // RGB -> 255, 255, 255
+    // HSV -> 360, 100, 100
+    // input: h, s, v -> 0 - 1.0
+    // output: r, g, b -> 0 - 1.0
+    // Refer to http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+    function HSVtoRGB(h : number, s : number, v : number) : Array.<number> {
+      var r = 0, g = 0, b = 0, i, f, p, q, t;
+      i = Math.floor(h * 6);
+      f = h * 6 - i;
+      p = v * (1 - s);
+      q = v * (1 - f * s);
+      t = v * (1 - (1 - f) * s);
+      switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+      }
+      return [r, g, b];
+    }
+
     // 関数自体をsetやclearするのではなく、
     // アップデート関数は回しっぱなしで、登録するデータを入れ替えて表示させる
     function update() : void {
@@ -124,7 +147,6 @@ class _Main {
     gl.uniform3fv(scaleLoc, new Float32Array([scale, scale, scale]));
 
     var colorLoc = gl.getUniformLocation(prog, 'color');
-    var color = [Math.random(), Math.random(), Math.random()];
 
     function render(f:number) : void {
       Timer.requestAnimationFrame(render);
@@ -162,9 +184,10 @@ class _Main {
     }
 
     // create data
+    var color = [] : Array.<number>;
     function generateData() : void {
       dt = 0;
-      color = [Math.random(), Math.random(), Math.random()];
+      color = HSVtoRGB(Math.random(), 1.0, 1.0);
       for (var i = 0; i < dataNum; i++) {
         weight.push(0.5 - Math.random() * 2);
         var vx0 = 0.01 - 0.02 * Math.random();
